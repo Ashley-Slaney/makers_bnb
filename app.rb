d53ef3784cf95584e05afa25275f995199ce7a16
LUNDI19 <michaelmcloughlin9@gmail.com>
2021-06-28 19:00:05 +0200

    spaces views and sessions params - working feature test

--- app.rb
+++ app.rb
@@ -3,6 +3,9 @@ require './lib/user'
 require './lib/space'
 
 class MakersBnB < Sinatra::Base 
+
+  enable :sessions
+
    get 'signup/new' do
      erb(:'signup/new') 
    end 
@@ -10,4 +13,21 @@ class MakersBnB < Sinatra::Base
    post '/signup' do 
 
    end 
+
+   get '/addnewspace' do
+    erb :new_space
+  end
+
+   post '/new_space' do
+    sessions[:name] = params[:name]
+    sessions[:description] = params[:description]
+    sessions[:price] = params[:price]
+    redirect '/spaces'
+   end
+
+   get '/spaces' do
+     erb :spaces
+   end
+
+
 end
