d96a5eb311712fc0afe1dba6205241cd3e4399c2
Ashley Slaney <ashleyslaney96@gmail.com>
2021-06-28 18:01:10 +0100

    sign up changes

--- app.rb
+++ app.rb
@@ -1,13 +1,25 @@
 require 'sinatra/base'
+require 'sinatra/reloader'
 require './lib/user'
 require './lib/space'
 
 class MakersBnB < Sinatra::Base 
-   get 'signup/new' do
-     erb(:'signup/new') 
-   end 
+  configure :development do
+    register Sinatra::Reloader
+  end
 
-   post '/signup' do 
+  get '/' do
+    erb(:index)
+  end
 
-   end 
+  post '/signup' do
+    User.sign_up(name: params[:name], email: params[:email], password: params[:password])
+    redirect '/spaces'
+  end 
+
+  get '/spaces' do
+
+  end
+
+  run! if app_file == $0
 end
--- spec/feature/signup_spec.rb
+++ spec/feature/signup_spec.rb
@@ -1,12 +1,13 @@
 require 'spec_helper'
 
 feature 'sign up' do
-    scenario 'user can sign up and sign in' do 
-        visit('signup/new')
-        fill_in('email', with: 'test@gmail.com')
-        fill_in('password', with: 'test123')
-        click_button('submit')
-        expect(page.current_path).to eq('/')
-        expect(page).to have_content('Welcome to Makers BnB, test@gmail.com')
-    end
+  scenario 'user can sign up and sign in' do 
+    visit('/')
+    fill_in('email', with: 'test@gmail.com')
+    fill_in('password', with: 'test123')
+    fill_in('confirmation', with: 'test123')
+    click_button('Sign up')
+    expect(page.current_path).to eq('/spaces')
+    expect(page).to have_content('Welcome to Makers BnB, test@gmail.com')
+  end
 end
